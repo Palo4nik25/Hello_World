faf4c8c43af7a92b5fb0f4dad9eb35c8d4c3225e
palo4nik25 <pawlo2005kaplan@gmail.com>
2023-06-15 12:40:31 +0200

    Changed file name to WeightSort

--- Hello World/src/WeightSort.java
+++ Hello World/src/WeightSort.java
@@ -1,7 +1,7 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Main {
+public class WeightSort {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         boolean playAgain = true;
