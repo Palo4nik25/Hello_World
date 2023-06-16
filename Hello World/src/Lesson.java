0a25411a6519a9e24529f6b1bd880203d8d7ae4f
palo4nik25 <pawlo2005kaplan@gmail.com>
2023-06-16 12:05:07 +0200

    Made an app, which will find average value from array, and find quantity of values, which are in boundaries from 60 to 80.

--- Hello World/src/Lesson.java
+++ Hello World/src/Lesson.java
@@ -1,5 +1,19 @@
+import java.util.Random;
+
 public class Lesson {
     public static void main(String[] args) {
+        float[] weight = new float[10];
+        float sum = 0f;
+        int count = 0;
 
+        for (int i = 0; i < weight.length; i++) {
+            weight[i] = (new Random().nextFloat(40, 100));
+            sum += weight[i];
+
+            if (weight[i] >= 60 && weight[i]<= 80) {
+                count++;
+            }
+        }
+        System.out.println("Average weight is: " + (sum / weight.length) + "\nPeople with weight from 60 to 80: " + count);
     }
 }
